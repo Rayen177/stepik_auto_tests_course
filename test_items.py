3cc617bc6b76917fe8f2f6c9f35c3ca4843dbe5b
Alexander <rayen177@gmail.com>
2023-04-22 23:24:23 +0300

    the solution for 3.6 exercise

---
+++ test_items.py
@@ -0,0 +1,19 @@
+import pytest
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+def should_be_element(browser):
+    try:
+        browser.find_element(By.CLASS_NAME, 'btn.btn-lg.btn-primary.btn-add-to-basket')
+    except NoSuchElementException:
+        return False
+    return True
+
+def test_page_contains_adding_button(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207'
+    browser.get(link)
+    element = should_be_element(browser)
+    assert element, 'Element button to add book to basket was not found'
+
+
+
